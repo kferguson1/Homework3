c97a15c38f49774d608a80f255d9c5525decb95a
Kiara Ferguson <Kiaraferguson16@yahoo.com>
2020-12-06 23:04:20 -0500

    Added letter array/lower case array

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Assigning Characters to Arrays
+var LetterCaseArray=[];
+var LowCase=[["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
