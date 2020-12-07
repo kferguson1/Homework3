811fa71945048f83f9e49a698a211d89e286bc19
Kiara Ferguson <Kiaraferguson16@yahoo.com>
2020-12-06 23:18:58 -0500

    Added number array

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var LetterCaseArray=[];
 var LowCase=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var UpperCase=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","X","Y","Z"]
 var Characters=["!","@","#","$","%","^","&","*","(",")","{","}","|","[","]",";","'",":","<",">","?","/"]
+var Numbers=["1","2","3","4","5","6","7","8","9","0"]
 
 // Write password to the #password input
 function writePassword() {
